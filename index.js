1c6013556c636db6cf10b0f9056d69a1c7795266
Rosendo Martinez <rosendomartinez270@gmail.com>
2022-06-22 14:18:56 -0500

    Implement code that creats and renders pixels

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const canvasContainer = document.querySelector('#canvas-container');
 
-function createCanvas(height, width, bgColor) {
+function createCanvas(canvasHeight, canvasWidth, bgColor, pixelColumns, pixelRows) {
     const canvas = document.createElement('div');
-    canvas.style.height = `${height}px`;
-    canvas.style.width = `${width}px`;
+    canvas.style.height = `${canvasHeight}px`;
+    canvas.style.width = `${canvasWidth}px`;
     canvas.style.backgroundColor = bgColor;
+    renderPixelsToCanvas(canvasHeight, canvasWidth, pixelColumns, pixelRows, canvas);
     return canvas;
 };
 
-function renderCanvas(canvasContainer, canvas) {
+function renderCanvasToDOMContainer(canvasContainer, canvas) {
     canvasContainer.appendChild(canvas);
+};
+
+function createPixels(canvasHeight, canvasWidth, pixelColumns, pixelRows) {
+    const pixelHeight = canvasHeight / pixelRows;
+    const pixelWidth = canvasWidth / pixelColumns;
+    const numberOfPixels = pixelColumns * pixelRows;
+    const pixels = new Array(numberOfPixels).fill('').map(() => createPixel(pixelWidth, pixelHeight));
+    return pixels;
+};
+
+function createPixel(pixelHeight, pixelWidth) {
+    const pixel = document.createElement('div');
+    pixel.style.height = `${pixelHeight}px`;
+    pixel.style.width = `${pixelWidth}px`;
+    return pixel;
+};
+
+function renderPixelsToCanvas(canvasHeight, canvasWidth, pixelColumns, pixelRows, canvas) {
+    const pixels = createPixels(canvasHeight, canvasWidth, pixelColumns, pixelRows);
+    for (let i = 0; i < pixels.length; i++) {
+        canvas.appendChild(pixels[i]);
+    };
 };
